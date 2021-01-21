bc4708c5d071d4d7e8be1ae004e08b7d1db2e143
aaronllcam <aaronllcam@gmail.com>
2021-01-21 16:38:40 +0100

    ngrx auth functions and getusers from firebase

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,7 +1,13 @@
+
 export class User{
     constructor(
         public uid: string,
         public name: string,
         public email: string
     ){}
+
+    static firebaseUserToUser({uid, name, mail}){
+        return new User(uid, name, mail)
+    }
+    
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,24 +1,38 @@
 import { Injectable } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { map } from "rxjs/operators";
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { map } from "rxjs/operators";
+import { Store } from '@ngrx/store';
+import { AppState } from '../app.reducer';
 import { User } from './../models/user.model';
+import * as authActions from './../auth/auth.actions';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  userSubscription: Subscription;
+
   constructor(public auth: AngularFireAuth,
-              private firestore: AngularFirestore) { }
+              private firestore: AngularFirestore,
+              private store: Store<AppState>) { }
 
   initAuthListener(){
     //Recuperamos toda la informacion del usuario.
     //Llamamos en esta funcion en el app.component, ya que el app.component se llama en todas laas vistas
-    this.auth.authState.subscribe( firebaseUser => {
-      console.log(firebaseUser)
-      console.log(firebaseUser?.uid)
-      console.log(firebaseUser?.email)
+    this.auth.authState.subscribe( (firebaseUser) => {
+      if(firebaseUser){
+        this.userSubscription = this.firestore.doc(`${firebaseUser.uid}/usuario`).valueChanges()
+                                    .subscribe( ({ uid, name, email }) => {
+                                      const user = new User(uid, name, email);
+                                      this.store.dispatch(authActions.setUser({ user }));
+                                    });
+      }else{
+        this.userSubscription.unsubscribe();
+        this.store.dispatch(authActions.unSetUser());
+      }
     });
   }
 
